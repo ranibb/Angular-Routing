222bc8ed156538ffb239994e840e3879d47267f7
Rani Abu Sultan <raniabusultan@gmail.com>
2018-09-02 23:22:53 +0300

    Optional Route Parameters

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -7,6 +7,10 @@ import { ActivatedRoute, Router, ParamMap } from '@angular/router'
       <h3>You selected department with id = {{departmentId}}</h3>
       <a (click)="goPrevious()">Previous</a>
       <a (click)="goNext()">Next</a>
+
+      <div>
+        <button (click)=gotoDepartments()>Back</button>
+      </div>
   `,
   styles: []
 })
@@ -32,6 +36,11 @@ export class DepartmentDetailComponent implements OnInit {
   goNext(){
     let nextId = this.departmentId + 1;
     this.router.navigate(['/departments', nextId])
+  }
+
+  gotoDepartments() {
+    let selectedId = this.departmentId ? this.departmentId : null;
+    this.router.navigate(['/departments', {id: selectedId, test: 'testvalue'}])
   }
 
 }
